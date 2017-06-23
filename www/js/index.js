2c476673a300f3d3b8f6bffbc444dcb83f7e6b9f
clChenLiang <clchenliang@hotmail.com>
2017-06-23 22:12:59 +0800

    使用H5写的页面，可以正确被加载
    JS与H5都能够通过字符串加载的方式获取得到。script 动态加载的时候，并不能通过 url 获取。这个可能是跟阿里云服务器的设置有关

--- www/js/index.js
+++ www/js/index.js
@@ -49,17 +49,30 @@ function loadRomoteWeb(){
         var data1 = '<div data-role="header" data-position="fixed" data-tap-toggle="false"><h1>扫描设置</h1></div><!-- /header --><div role="main" class="ui-content"><div data-role="controlgroup" data-type="horizontal"><a data-role="button" id="startScan" data-inline="true" class="ui-btn-inline">开始扫描</a><a data-role="button" id="stopScan" data-inline="true" class="ui-btn-inline">停止扫描</a><a data-role="button" id="findAssociat" data-inline="true" class="ui-btn-inline">绑定扫描</a></div><p>   </p><div><ul data-role="listview" id="deviceList"><!--<li class="devicePrepar ui-li-has-alt "><a href="#" class="ui-btn"><h3>CSRMESHlight</h3><td>00:23:32:67:89</td></a><a href="#controlPage" name="kkk" class="connect ui-btn ui-btn-icon-notext ui-icon-action" ></a></li>--></ul></div></div><!-- /content --><div data-role="footer" data-position="fixed" data-tap-toggle="false"><div data-role="navbar"><ul><li><a href="#">扫描</a></li><li><a href="#controlPage"class="controlPageButton">控制</a></li><li><a href="#settingPage" class="settingBottom">设置</a></li></ul></div></div>';
         var data2 = '<div><h1>扫描设置</h1></div><!-- /header --><div ><div><a class="ui-btn-inline">开始扫描</a><a class="ui-btn-inline">停止扫描</a><a class="ui-btn-inline">绑定扫描</a></div><p>   </p><div><ul><!--<li class="devicePrepar ui-li-has-alt "><a href="#" class="ui-btn"><h3>CSRMESHlight</h3><td>00:23:32:67:89</td></a><a href="#controlPage" name="kkk" class="connect ui-btn ui-btn-icon-notext ui-icon-action" ></a></li>--></ul></div></div><!-- /content --><div ><div ><ul><li><a href="#">扫描</a></li><li><a href="#controlPage"class="controlPageButton">控制</a></li><li><a href="#settingPage" class="settingBottom">设置</a></li></ul></div></div>';
 
-        var data = "<div style='background:red'>abcdsadfa</div>";// 可生成
+        //var data = "<div style='background:red'>abcdsadfa</div>";// 可生成
         //$("body").innerHTML = data;
         //document.getElementsByTagName("body")[0].innerHTML = data;// 可生成
         //var data = '<div class="chenliang">陈亮</div>';//可以
-        var data = '<div class="chenliang">陈亮</div>';//可以,加样式测试 --- 成功
+        //var data = '<div class="chenliang">陈亮</div>';//可以,加样式测试 --- 成功
         //document.body.innerHTML = data1; // 成功，准备测试，是否没有将jqm 的css样式加载进来
         //document.body.innerHTML = data2;// 测试有无加载上 JQM 的样式;略微有所不同，猜测可能是class也删除的原因；
         //document.body.innerHTML = data1;
         //$('body').listview('refresh');
         //$("body").innerHTML = data;// 好像可用性不好，不能使用
+        //var myDiv = document.createElement('div');
+        //myDiv.innerHTML = data1;
+        //document.body.appendChild(myDiv);
         console.log("加载页面动作启动");
+
+        // 使用普通H5编写页面
+        var data = '<div style="width: 100%;height: 100%;"><label id="show" style="width:80%;height: 30%;">显示内容</label><button id="scan">扫描</button><button id="conn">连接</button><button id="asso">绑定</button> </div>';
+        document.body.innerHTML = data;
+        // JS 代码也靠解析出来
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        //script.src = "47.93.228.84:3315/js/onclick.js";
+        script.appendChild(document.createTextNode('$(document).on("click","#scan",function(){ $("#show").val("点击"); try{ csrmesh.startScan(function(){console.log("成功点击")}, function () { console.log("error 点击");  }); }catch(e){ console.log(e);}})'));
+        document.body.appendChild(script);
     }catch(e){
         console.log("加载页面出错 ："+e);
     }
